b2cc6190bb2d19d1bfa8953a514edac7d722d948
King-Of-Programer <alexdichemix@gmail.com>
2023-04-08 18:32:08 +0300

    api Search Recipes by Ingredients

--- DietPlanner/MainWindow.xaml.cs
+++ DietPlanner/MainWindow.xaml.cs
@@ -14,6 +14,12 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Net.Http;
 
+using System.Net.Http.Headers;
+
+using Newtonsoft.Json;
+using System.Security.Policy;
+
+
 namespace DietPlanner
 {
     /// <summary>
@@ -21,32 +27,43 @@ namespace DietPlanner
     /// </summary>
     public partial class MainWindow : Window
     {
-        public string api_key = "bf72237be72d44549d0acc3588ce6dfb";
+        private readonly HttpClient _client = new HttpClient();
+        private readonly string _apiKey = "bf72237be72d44549d0acc3588ce6dfb";
+
         public MainWindow()
         {
             InitializeComponent();
-            GetRecipesAsync();
-
-
-
         }
-        private async Task GetRecipesAsync()
-        {
-            using (var client = new HttpClient())
-            {
-                var url = $"https://api.spoonacular.com/recipes/complexSearch?apiKey={api_key}";
-                var response = await client.GetAsync(url);
-                var content = await response.Content.ReadAsStringAsync();
 
-                if (response.IsSuccessStatusCode)
+        private async void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            var url = $"https://api.spoonacular.com/recipes/findByIngredients?apiKey={_apiKey}&ingredients={IngredientsTextBox}";
+            try
+            { 
+                var response = await _client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                var content = await response.Content.ReadAsStringAsync();
+                dynamic data = JsonConvert.DeserializeObject<dynamic>(content);
+
+                //Console.WriteLine(data);
+
+
+
+                foreach (var missedIngredient in data)
                 {
-                    MessageBox.Show("Connected");
+                    string tmp = missedIngredient.title;
+                    names.Text += tmp + "\n";
+                    
+
                 }
-                else
-                {
-                    MessageBox.Show("No Connect");
-                }
+
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
             }
         }
     }
 }
+
---
+++ DietPlanner/Recipe.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DietPlanner
+{
+    public class MissedIngredient
+    {
+        public int id { get; set; }
+        public double amount { get; set; }
+        public string unit { get; set; }
+        public string unitLong { get; set; }
+        public string unitShort { get; set; }
+        public string aisle { get; set; }
+        public string name { get; set; }
+        public string original { get; set; }
+        public string originalName { get; set; }
+        public List<object> meta { get; set; }
+        public string image { get; set; }
+    }
+
+    public class Recipe
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string image { get; set; }
+        public string imageType { get; set; }
+        public int usedIngredientCount { get; set; }
+        public int missedIngredientCount { get; set; }
+        public List<MissedIngredient> missedIngredients { get; set; }
+    }
+}
