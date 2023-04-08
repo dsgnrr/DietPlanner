4d83ccec797da81e868e6ab62f06051c32e8b7ac
King-Of-Programer <alexdichemix@gmail.com>
2023-04-08 13:21:12 +0300

    connect to api

--- DietPlanner/MainWindow.xaml.cs
+++ DietPlanner/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Http;
 
 namespace DietPlanner
 {
@@ -20,9 +21,32 @@ namespace DietPlanner
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string api_key = "bf72237be72d44549d0acc3588ce6dfb";
         public MainWindow()
         {
             InitializeComponent();
+            GetRecipesAsync();
+
+
+
+        }
+        private async Task GetRecipesAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                var url = $"https://api.spoonacular.com/recipes/complexSearch?apiKey={api_key}";
+                var response = await client.GetAsync(url);
+                var content = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show("Connected");
+                }
+                else
+                {
+                    MessageBox.Show("No Connect");
+                }
+            }
         }
     }
 }
